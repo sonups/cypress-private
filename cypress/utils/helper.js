a074151da91d7830057b17c440730f1cfee83f1e
sonups <sonu.sadasivan@gmail.com>
2021-03-08 18:28:13 +0530

    Added

---
+++ cypress/utils/helper.js
@@ -0,0 +1,38 @@
+
+export const getArrayRandomelement = (arr) => {
+  if (arr && arr.length) {
+    return arr[Math.floor(Math.random() * arr.length)]
+  }
+}
+
+export const randomGenerator = (number) => {
+  return Math.floor(Math.random() * (number))
+}
+
+export const selectRandomDropdownValue = (dropdownelement, random_option) => {
+  cy.get(random_option).its('length').then(($length) => {
+    const list = randomGenerator($length)
+
+    cy.get(random_option)
+            .eq(list)
+            .then((elem) => {
+              cy.get(dropdownelement)
+                    .select(elem.val())
+            })
+  })
+}
+
+export const clickRandomElementByIndex = (randomelement) => {
+  cy.get(randomelement).its('length').then(($length) => {
+    const list = randomGenerator($length)
+
+    console.log('listno', list)
+    cy.get(randomelement)
+            .eq(list)
+            .then(($elem) => {
+              cy.wrap($elem).click()
+            })
+  })
+}
+
+
